ad0a94861f9fd737d3a2cb3139b268bad087d4a1
usermilton <115846140+usermilton@users.noreply.github.com>
2022-10-28 18:54:00 -0500

    Update multiplicar.php

--- multiplicar.php
+++ multiplicar.php
@@ -1,3 +1,4 @@
+parte 1
 <?php
      for ($column = 1; $column <= 7 ; $column++) {
          echo $column;
@@ -18,3 +19,31 @@
         }
     }
 ?>
+
+parte 2
+
+<?php
+     for ($column = 1; $column <= 7 ; $column++) 
+         echo nl2br("\t $column");
+
+    for ($row = 1; $row <= 7 ; $row++){
+        
+        echo nl2br("\n $row");
+        
+        for ($column = 1; $column <= 7 ; $column++){
+            $result = $column;
+
+            if ($column === 1) {
+                $result = $row;
+              }
+
+            if ($column >= 1) {
+                $result = $row * $column;
+              }
+            echo nl2br("\t $result");
+      
+        }
+    }
+?>
+
+parte 3
